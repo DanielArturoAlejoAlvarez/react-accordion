2ca09280ce932620a466b400b555fb3e1ebd31d5
DanielArturoAlejoAlvarez <yourmediasoft@gmail.com>
2020-12-18 13:40:07 -0500

    Adding react-icons and Implement our components

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import { AccordionSection, Container } from './AccordionElems'
+import { IconContext } from 'react-icons'
+import { Data } from './Data'
 
 const Accordion = () => {
   return (
-    <AccordionSection>
-      <Container>
-
-      </Container>
-    </AccordionSection>
+    <IconContext.Provider value={{color: "#00FF89"}}>
+      <AccordionSection>
+        <Container>
+          {Data.map((item,index)=>(
+            
+          ))}
+        </Container>
+      </AccordionSection>
+    </IconContext.Provider>
+    
   )
 }
 
