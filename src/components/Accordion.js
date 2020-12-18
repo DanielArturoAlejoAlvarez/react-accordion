03f245a5e1faafe7c1250d351b876b29c1a87410
DanielArturoAlejoAlvarez <yourmediasoft@gmail.com>
2020-12-18 14:25:11 -0500

    Creating application logic

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,17 +1,30 @@
-import React from 'react'
-import { AccordionSection, Container } from './AccordionElems'
+import React, { useState } from 'react'
+import { AccordionSection, Container, Wrap } from './AccordionElems'
 import { IconContext } from 'react-icons'
 import { Data } from './Data'
 import { FiPlus, FiMinus } from 'react-icons/fi'
 
 const Accordion = () => {
+
+  const [clicked, setClicked] = useState(false)
+
+  const toggle = (index)=>{
+    if (clicked === index) {
+      return setClicked(null)
+    }
+    setClicked(index)
+  }
+
   return (
     <IconContext.Provider value={{color: "#00FF89"}}>
       <AccordionSection>
         <Container>
           {Data.map((item,index)=>(
             <>
-              <h1>{item.question}</h1>
+              <Wrap onClick={()=>toggle(index)} key={index}>
+                <h1>{item.question}</h1>
+                <span>{clicked === index ? <FiMinus /> : <FiPlus />}</span>
+              </Wrap>
               <p>{item.answer}</p>
             </>
           ))}
--- src/components/AccordionElems.js
+++ src/components/AccordionElems.js
@@ -1,8 +1,32 @@
 import styled from 'styled-components'
 
 export const AccordionSection = styled.section`
-
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  background: #FFF;
+  height: 100vh;
 `
-export const Container = styled.section`
-
+export const Container = styled.div`
+  position: absolute;
+  top: 30%;
+  box-shadow: 2px 10px 35px 1px rgba(153,153,153,.3);
 `
+export const Wrap = styled.div`
+  background: #272727;
+  color: #FFF;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  text-align: center;
+  cursor: pointer;
+
+  h1 {
+    padding: 2rem;
+    font-size: 2rem;
+  }
+`
+
