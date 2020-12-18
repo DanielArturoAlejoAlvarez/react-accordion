0aeae9c8429ac1b2126367de963270eb6d601e1a
DanielArturoAlejoAlvarez <yourmediasoft@gmail.com>
2020-12-18 13:50:30 -0500

    Adding our Provider Context value controls and Mapping data

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { AccordionSection, Container } from './AccordionElems'
 import { IconContext } from 'react-icons'
 import { Data } from './Data'
+import { FiPlus, FiMinus } from 'react-icons/fi'
 
 const Accordion = () => {
   return (
@@ -9,7 +10,10 @@ const Accordion = () => {
       <AccordionSection>
         <Container>
           {Data.map((item,index)=>(
-            
+            <>
+              <h1>{item.question}</h1>
+              <p>{item.answer}</p>
+            </>
           ))}
         </Container>
       </AccordionSection>
